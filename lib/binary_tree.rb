43e598bc2887948d5b5354b9db8bec4cbbf637a7
Pauloparakleto <user.email>
2024-03-02 11:37:49 -0300

    feat binary_tree.rb add insert recursive way.

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -5,7 +5,7 @@
 class BinaryTree
   attr_accessor :root
 
-  def initialize(node)
+  def initialize(node=Node.new)
     @root = node
   end
 
@@ -21,21 +21,31 @@ class BinaryTree
     sub_root.value
   end
 
-  def possibilities(morse)
-    sub_node = root
-    stack_nodes = []
-    values = []
-    morse.split('') do |pulse|
-      if pulse.eql?('.')
-        sub_node = sub_node.left
-        stack_nodes << sub_node
-        values << sub_node.value
-      end
-    end
+  def insert(number)
+    return root.value = number if root.value.nil?
 
-    values
+    new_node = Node.new(number)
+
+    insert_at_node(root, new_node)
   end
 
   class Error < StandardError; end
   # Your code goes here...
+  private
+
+  def insert_at_node(tree_node, new_node)
+    return new_node if tree_node.nil?
+
+    return tree_node if tree_node.value == new_node.value
+
+    if new_node.value < tree_node.value
+      return insert_at_node(tree_node.left, new_node) unless tree_node.left.nil?
+
+      return tree_node.left = new_node
+    end
+
+    return insert_at_node(tree_node.right, new_node) unless tree_node.right.nil?
+
+    tree_node.right = new_node
+  end
 end
