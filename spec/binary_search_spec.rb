4b548959d037c2633a73e4492db7a47a41734ade
Pauloparakleto <user.email>
2024-03-04 19:09:31 -0300

    test binary_search_spec.rb: describe #recursive_search

--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -29,4 +29,31 @@ RSpec.describe BinarySearch do
       end
     end
   end
+
+  describe '#recursive_search' do
+    context 'when found' do
+
+      it 'finds index 5' do
+        expect(described_class.new(sorted_array, 11).recursive_search).to eq(5)
+      end
+
+      it 'finds index 0' do
+        expect(described_class.new(sorted_array, 1).recursive_search).to eq(0)
+      end
+
+      it 'finds index 3' do
+        expect(described_class.new(sorted_array, 7).recursive_search).to eq(3)
+      end
+
+      it 'finds index 2' do
+        expect(described_class.new(sorted_array, 5).recursive_search).to eq(2)
+      end
+    end
+
+    context 'when not found' do
+      it 'returns -1' do
+        expect(described_class.new(sorted_array, 99).recursive_search).to eq(-1)
+      end
+    end
+  end
 end
