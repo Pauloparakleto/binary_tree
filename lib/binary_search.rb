3a2b81d02f7a2e6938e8af3930fccb2e73af0fd0
Pauloparakleto <user.email>
2024-03-04 19:10:58 -0300

    feat binary_search.rb: add #recursive_search

--- lib/binary_search.rb
+++ lib/binary_search.rb
@@ -24,4 +24,17 @@ class BinarySearch
 
     INDEX_NOT_FOUND
   end
+
+  def recursive_search(left=0, right=((array.length) - 1))
+    return INDEX_NOT_FOUND if left > right
+
+    midst_index = (left + right) / 2
+    return midst_index if number.eql?(array[midst_index])
+
+    if number < array[midst_index]
+      recursive_search(left, midst_index - 1)
+    else
+      recursive_search(midst_index + 1, right)
+    end
+  end
 end
