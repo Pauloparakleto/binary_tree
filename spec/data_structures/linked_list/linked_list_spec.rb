dd73a996b13bf05400d86253290277bab8128466
Pauloparakleto <alparakleto@gmail.com>
2024-05-12 14:21:54 -0300

    chore linked_list_spec.rb: add space after variable instanciation.

--- spec/data_structures/linked_list/linked_list_spec.rb
+++ spec/data_structures/linked_list/linked_list_spec.rb
@@ -1,29 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe LinkedList do
- describe '#new' do
-   let(:head_node){ LinkedList::Node.new('A head value') }
-   let(:wrong_class) { Hash.new }
+	describe '#new' do
+  let(:head_node){ LinkedList::Node.new('A head value') }
+  let(:wrong_class) { Hash.new }
 
-   context 'with node argument' do
-     it 'is truthy' do
-       expect(described_class.new(head_node)).to be_truthy
+  context 'with node argument' do
+    it 'is truthy' do
+      expect(described_class.new(head_node)).to be_truthy
+    end
+  end
+
+  context 'with a wrong class as node argument' do
+    let(:error_message ){ 'only LinkedList::Node must be provided' }
+
+    it 'raises error WrongArgument with message' do
+      expect{ described_class.new(wrong_class) }
+        .to raise_error(ArgumentError, error_message)
+    end
+  end
+
+  context 'without node argument' do
+    it 'is truthy' do
+      expect(described_class.new).to be_truthy
      end
    end
-
-   context 'with a wrong class as node argument' do
-     let(:error_message ){ 'only LinkedList::Node must be provided' }
-     it 'raises error WrongArgument with message' do
-       expect{ described_class.new(wrong_class) }
-         .to raise_error(ArgumentError, error_message)
-     end
-   end
-
-   context 'without node argument' do
-     it 'is truthy' do
-       expect(described_class.new).to be_truthy
-     end
-   end
- end
+  end
 end
 
