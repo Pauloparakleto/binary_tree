a05e1df3b9990cf434d12cce8b2cc68f162f68dd
Pauloparakleto <alparakleto@gmail.com>
2024-05-13 19:28:12 -0300

    refactor linked list. Do not allow Node class(or any class) as initial argument

--- lib/data_structures/linked_list/linked_list.rb
+++ lib/data_structures/linked_list/linked_list.rb
@@ -1,19 +1,19 @@
 class LinkedList
   attr_accessor :node, :head
 
-  def initialize(node = nil)
-    @head = node
-    if !head.is_a?(LinkedList::Node) && !node.nil?
-      raise ArgumentError, 'only LinkedList::Node must be provided'
+  def initialize(value = nil)
+    @head = LinkedList::Node.new(value)
+    if value.nil?
+      raise ArgumentError, 'You must provide a value'
     end
   end
 
-  def append(node)
+  def append(value)
     current_node = head
     while !current_node.next_node.nil?
       current_node = current_node.next_node
     end
 
-    current_node.next_node = node
+    current_node.next_node = LinkedList::Node.new(value)
   end
 end
--- spec/data_structures/linked_list/linked_list_spec.rb
+++ spec/data_structures/linked_list/linked_list_spec.rb
@@ -1,49 +1,40 @@
 require 'spec_helper'
 
 RSpec.describe LinkedList do
-  let(:head_node){ LinkedList::Node.new('A head value') }
+  let(:head_value){ 'A head value' }
 
 	describe '#new' do
    let(:wrong_class) { Hash.new }
 
     context 'with node argument' do
       it 'is truthy' do
-        expect(described_class.new(head_node)).to be_truthy
-      end
-    end
-
-    context 'with a wrong class as node argument' do
-      let(:error_message ){ 'only LinkedList::Node must be provided' }
-
-      it 'raises error WrongArgument with message' do
-        expect{ described_class.new(wrong_class) }
-          .to raise_error(ArgumentError, error_message)
+        expect(described_class.new(head_value)).to be_truthy
       end
     end
 
     context 'without node argument' do
-      it 'is truthy' do
-        expect(described_class.new).to be_truthy
+      it 'raises error' do
+        expect{ described_class.new }.to raise_error(ArgumentError, 'You must provide a value')
       end
     end
   end
 
   describe '#append(value)' do
     context 'when append value' do
-      let(:next_node){ LinkedList::Node.new('next node value') }
+      let(:next_value){ 'next node value' }
 
       it 'keeps head as node_head' do
-        linked_list = described_class.new(head_node)
-        linked_list.append(next_node)
+        linked_list = described_class.new(head_value)
+        linked_list.append(next_value)
 
-        expect(linked_list.head).to eq(head_node)
+        expect(linked_list.head.value).to eq(head_value)
       end
 
       it 'has next node set as next_node' do
-        linked_list = described_class.new(head_node)
-        linked_list.append(next_node)
+        linked_list = described_class.new(head_value)
+        linked_list.append(next_value)
 
-        expect(linked_list.head.next_node).to eq(next_node)
+        expect(linked_list.head.next_node.value).to eq(next_value)
       end
     end
   end
