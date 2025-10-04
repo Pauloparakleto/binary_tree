5fbfd821b8c79c17a6010bb5f8c98147edaedb54
Pauloparakleto <alparakleto@gmail.com>
2025-10-04 18:54:59 -0300

    binary tree in order walk tree
    List tree values from left-head-right order.

---
+++ lib/data_structures/b_tree.rb
@@ -0,0 +1,28 @@
+module Binary
+  class Node
+    attr_accessor :left, :right, :value
+
+    def initialize(value = nil)
+      @left = nil
+      @right = nil
+      @value = value
+    end
+  end
+end
+
+class BTree
+  attr_accessor :head, :current_node
+
+  def initialize(head = nil)
+    @head = Binary::Node.new
+    @values = []
+  end
+
+  def in_order(node = @head)
+    return @values if node.nil?
+
+    in_order(node.left)
+    @values << node.value
+    in_order(node.right)
+  end
+end
