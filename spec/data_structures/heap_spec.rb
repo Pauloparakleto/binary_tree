11c8e5155bc9a50fc625d19f8f0774876468453e
Pauloparakleto <alparakleto@gmail.com>
2025-10-06 07:18:07 -0300

    refactor heap test with context

--- spec/data_structures/heap_spec.rb
+++ spec/data_structures/heap_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe Heap do
+  subject(:heap) { described_class.new(node) }
+  let(:node) { HeapTree::Node.new(4) }
+
   it 'is truthy' do
     expect(described_class.new).to be_truthy
   end
 
-  it 'max heapify' do
-    node = HeapTree::Node.new(4)
-    heap = described_class.new(node)
-    heap.head.right = HeapTree::Node.new(2)
-    heap.head.left = HeapTree::Node.new(8)
-    heap.head.left.parent = node
-    heap.head.right.parent = node
+  context 'when max is 8 on left' do
+    before do
+      heap.head.right = HeapTree::Node.new(2)
+      heap.head.left = HeapTree::Node.new(8)
+      heap.head.left.parent = node
+      heap.head.right.parent = node
+    end
 
-    heap.max_heapify
+    it 'max heapify' do
 
-    expect(heap.head.value).to eq(8)
-    expect(heap.head.left.value).to eq(4)
-    expect(heap.head.right.value).to eq(2)
+      heap.max_heapify
+
+      expect(heap.head.value).to eq(8)
+      expect(heap.head.left.value).to eq(4)
+      expect(heap.head.right.value).to eq(2)
+    end
   end
 end
