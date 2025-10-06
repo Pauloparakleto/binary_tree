b29540bbb2c3d984de7a4c82a9c5b844e3eaf5b5
Pauloparakleto <alparakleto@gmail.com>
2025-10-05 21:30:25 -0300

    Add max_heapofy to heap tree the simple case where there is just 3 nodes.

--- lib/data_structures/heap.rb
+++ lib/data_structures/heap.rb
@@ -3,4 +3,18 @@ class Heap < BTree
     @head = head
     super(@head)
   end
+
+  def max_heapify(node = @head.left)
+    return if node.nil?
+
+    current = node
+    parent = current.parent
+    max_value = current.value
+    if parent.value < current.value
+      parent_value = node.parent.value
+      value = current.value
+      parent.value = value
+      current.value = parent_value
+    end
+  end
 end
--- spec/data_structures/heap_spec.rb
+++ spec/data_structures/heap_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe Heap do
   it 'max heapify' do
     node = HeapTree::Node.new(4)
     heap = described_class.new(node)
-    heap.head.left = HeapTree::Node.new(2)
-    heap.head.right = HeapTree::Node.new(8)
+    heap.head.right = HeapTree::Node.new(2)
+    heap.head.left = HeapTree::Node.new(8)
+    heap.head.left.parent = node
+    heap.head.right.parent = node
 
     heap.max_heapify
 
     expect(heap.head.value).to eq(8)
-    expect(heap.head.left.value).to eq(2)
-    expect(heap.head.right.value).to eq(4)
+    expect(heap.head.left.value).to eq(4)
+    expect(heap.head.right.value).to eq(2)
   end
 end
