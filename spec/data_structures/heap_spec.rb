5499ba763cc3c4ed4616c2f83871f99731d20cab
Pauloparakleto <alparakleto@gmail.com>
2025-10-06 07:22:12 -0300

    test when heap has max value on right

--- spec/data_structures/heap_spec.rb
+++ spec/data_structures/heap_spec.rb
@@ -25,4 +25,21 @@ RSpec.describe Heap do
       expect(heap.head.right.value).to eq(2)
     end
   end
+
+  context 'when max is 16 on right' do
+    before do
+      heap.head.right = HeapTree::Node.new(16)
+      heap.head.left = HeapTree::Node.new(8)
+      heap.head.left.parent = node
+      heap.head.right.parent = node
+    end
+
+    it 'max heapify' do
+      heap.max_heapify
+
+      expect(heap.head.value).to eq(16)
+      expect(heap.head.left.value).to eq(4)
+      expect(heap.head.right.value).to eq(8)
+    end
+  end
 end
