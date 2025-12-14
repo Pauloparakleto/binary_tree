1379f3ddea101809f8addf8700e68648d4862518
Pauloparakleto <alparakleto@gmail.com>
2025-12-14 13:46:13 -0300

    add #value_of_left_child

--- lib/data_structures/heap.rb
+++ lib/data_structures/heap.rb
@@ -31,4 +31,18 @@ class Heap
 
     list[left_child_index]
   end
+
+  def index_of_right_child(index)
+    right_child_index = (2 * index) + 2
+    return if right_child_index > list.length - 1
+
+    right_child_index
+  end
+
+  def value_of_right_child(index)
+    right_child_index = index_of_right_child(index)
+    return if right_child_index.nil?
+
+    list[right_child_index]
+  end
 end
